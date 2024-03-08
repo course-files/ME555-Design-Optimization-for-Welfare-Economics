d4ff0f1aad59f69b698a93d9c65d4ef811d0f5f2
Course Files <137632706+course-files@users.noreply.github.com>
2024-03-07 20:09:27 -0500

    Added cost of energy to the objective function

--- Subsystem3-Crop-Production/Baseline/random-search.py
+++ Subsystem3-Crop-Production/Baseline/random-search.py
@@ -3,8 +3,8 @@
 #
 # Course Code: ME555
 # Course Name: Design Optimization
-# Semester Duration: 10th Jan 2024 to 29th April 2024
 # University of Michigan
+# Semester Duration: 10th Jan 2024 to 29th April 2024
 # 
 ## Purpose: ----
 # Perform function optimization using a random search (a naive optimization
@@ -17,13 +17,14 @@ import numpy as np
 
 
 # Objective Function
-def objective(r, cw, T):
-    return r ** -1.0 - math.sin(cw / T)
+def objective(r, cw, T, ce):
+    return r ** -1.0 - (math.sin(cw / T) + ce)
 
 
 # Constraints
 r_min, r_max = 0.0, 25000.0
 cw_min, cw_max = 400.0, 5000.0
+ce_min, ce_max = 300.0, 9000.0
 
 # Parameters
 T = 3
@@ -32,16 +33,18 @@ T = 3
 # Generate random samples within the design space
 r_design_space = r_min + np.random.rand(100) * (r_max - r_min)
 cw_design_space = cw_min + np.random.rand(100) * (cw_max - cw_min)
+ce_design_space = ce_min + np.random.rand(100) * (ce_max - ce_min)
 
 # Range Space
 # Evaluate the objective function for each sample
-range_space = np.array([objective(r, cw, T) 
-                        for r, cw in zip(r_design_space, cw_design_space)])
+range_space = np.array([objective(r, cw, T, ce) 
+                        for r, cw, ce in zip(r_design_space, cw_design_space,
+                                             ce_design_space)])
 
 # Identify the index of the optimum result
 optimum_index = np.argmin(range_space)
 
 # Print the Result
-print('Optimum: f(r=%.5f, cw=%.5f) = %.5f' % 
+print('Optimum: f(r=%.5f, cw=%.5f, ce=%.5f) = %.5f' % 
       (r_design_space[optimum_index], cw_design_space[optimum_index], 
-       range_space[optimum_index]))
+       ce_design_space[optimum_index], range_space[optimum_index]))
