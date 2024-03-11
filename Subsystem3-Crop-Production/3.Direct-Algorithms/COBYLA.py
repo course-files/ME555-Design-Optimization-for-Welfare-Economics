3e3e27310b570ccd715fd99140355b72f4d689c0
Course Files <137632706+course-files@users.noreply.github.com>
2024-03-11 19:18:09 -0400

    Updated objective function (before curve fitting) and added all constraints

--- Subsystem3-Crop-Production/3.Direct-Algorithms/COBYLA.py
+++ Subsystem3-Crop-Production/3.Direct-Algorithms/COBYLA.py
@@ -23,23 +23,24 @@ def objective_function(x, grad):
     Parameter 'grad' is for gradient, which we don't use with DIRECT, but it's part of the function signature.
     r = x[0]
     cw = x[1]
-    t = x[2]
+    tw = x[2]
     ce = x[3]
     qs_stddev = x[4]
     qd = x[5]
     qs = x[6]
-    mal = x[7]
-    tal = x[8]
+    ts = x[7]
+    mal = x[8]
+    tal = x[9]
     """
-    return (1 * (x[0] ** -1.0) - 1 * (1 * (np.sin(x[1] / x[2])) ** 2/3 * 1 * (x[3]) ** 1/3) +
-            (x[4]) ** 2 + 1 * (x[5]) + 1 * (x[6] - x[5]) +
-            1 * (x[7] ** -1) - 1 * (x[8] ** -1))
+    return (1 * (x[0] ** -1.0) - 1 * ((np.sin(x[1] / x[2])) ** 1/3 * (x[3]) ** 2/3) +
+            (x[4]) ** 1 + 1 * (x[5]) ** -1.0 + 1 * ((np.sin(x[6] / x[7])) - x[5]) +
+            1 * (x[8] ** -1.0) - 1 * (x[9] ** -1))
 
 
 # ==== Constraint Functions ====
 # Inequality constraint function in the form g(x) <= 0
 def g1(x, grad):
-    return x[7] - x[0]
+    return x[9] - x[0]
 
 
 # Inequality constraint function in the form g(x) <= 0
@@ -47,28 +48,53 @@ def g2(x, grad):
     return np.sin(x[1] / x[2]) + x[3] - x[0]
 
 
+# Inequality constraint function in the form g(x) <= 0
+def g3(x, grad):
+    return (0.1 / x[6]) - x[4]
+
+
+# Inequality constraint function in the form g(x) <= 0
+def g4(x, grad):
+    return (0.15 / x[0]) - x[8]
+
+
 # Equality constraint function in the form h(x) = 0
 def h1(x, grad):
     return x[6] - x[5]
 
+# Equality constraint function in the form h(x) = 0
+def h2(x, grad):
+    return x[2] - 3
+
+
+# Equality constraint function in the form h(x) = 0
+def h3(x, grad):
+    return 5 - x[7]
+
 
 # ==== Constraint Bounds ====
-lower_bounds = [1, 400, 3, 300, 1, 1, 1, 800, 1]
-upper_bounds = [25000, 5000, 3, 9000, 750, 2500, 2500, 1500, 15000]
+lower_bounds = [46700, 20, 1, 2000, 0, 720, 720, 1, 50, 5000]
+upper_bounds = [70049, 5000, 12, 10000, 180, 1080, 1080, 12, 500, 20000]
 
 # ==== Optimizer Object ====
-# Create an optimizer object with 9 dimensions for COBYLA
-opt = nlopt.opt(nlopt.LN_COBYLA, 9)
+# Create an optimizer object with 10 dimensions for COBYLA
+opt = nlopt.opt(nlopt.LN_COBYLA, 10)
 
 # Set the objective function
 opt.set_min_objective(objective_function)
 
-# Add the inequality constraints with a tolerance for how closely it must be met
+# Add the inequality constraints
+# with a tolerance for how closely they must be met
 opt.add_inequality_constraint(g1, 1e0)
 opt.add_inequality_constraint(g2, 1e0)
+opt.add_inequality_constraint(g3, 1e0)
+opt.add_inequality_constraint(g4, 1e0)
 
-# Add the equality constraint.
+# Add the equality constraints
+# with a tolerance for how closely they must be met
 opt.add_equality_constraint(h1, 1e0)
+opt.add_equality_constraint(h2, 1e0)
+opt.add_equality_constraint(h3, 1e0)
 
 # Set the lower bounds and upper bounds
 opt.set_lower_bounds(lower_bounds)
@@ -80,7 +106,7 @@ opt.set_maxeval(1000)
 
 # ==== Perform the Optimization ====
 # Initialization point
-x_opt = opt.optimize([1, 400, 3, 300, 1, 1, 1, 800, 1])
+x_opt = opt.optimize([46700, 20, 1, 2000, 0, 720, 720, 1, 50, 5000])
 min_f = opt.last_optimum_value()
 
 # print(f"Optimal solution found: {x_opt}, with minimum value: {min_f}")
