e5acec950e2d416c13ad69c204894c9aa157d944
Course Files <137632706+course-files@users.noreply.github.com>
2024-03-16 21:24:12 -0400

    Update phase-shift.py

--- Subsystem3-Crop-Production/1.Curve-Fitting/phase-shift.py
+++ Subsystem3-Crop-Production/1.Curve-Fitting/phase-shift.py
@@ -1,12 +1,16 @@
 from math import pi
 
+
 T = 6  # Total duration of the cycle in months
 B = 2 * pi / T  # B value based on the period T
+
 
 # Target month for the peak is month 6
 target_peak_month = 6
 
-# Solving for phase_shift
-phase_shift = target_peak_month - (pi/2) / B
 
-print("Phase shift = ",phase_shift)
+# Solving for phase
+phase = target_peak_month - (pi/2) / B
+
+
+print("Phase = ", phase)
